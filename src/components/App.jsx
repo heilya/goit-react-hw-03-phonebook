a45576ccfe08f919258e61f62c673c1b0757477c
heilya <123752915+heilya@users.noreply.github.com>
2023-08-21 10:21:38 +0200

    task 1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,25 @@ import { ContactList } from "./contactlist/contactlist";
 
 export class App extends Component {
   state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [],
     filter: '',
   };
+
+componentDidUpdate(prevState) {
+    if (prevState.contacts!== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    };
+  };
+  
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({contacts: parsedContacts});
+    };
+  };
+
+
 
   handleSubmitForm = ({name,number}) =>{
     if(this.state.contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
